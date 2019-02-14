5d18166880884b6e32df1bdf7ab504ad3438d0d5
Edinei <galera.org@gmail.com>
2019-02-13 22:58:02 -0200

    new entitys frete and create user in agente create

---
+++ public/react/agentes/update.php
@@ -0,0 +1,26 @@
+<?php
+
+if(empty($dados['usuario_id']) && !empty($dados['senha'])) {
+    $user = [
+        "nome" => $dados['agente'],
+        "email" => $dados['email'],
+        "password" => $dados['senha'],
+        "setor" => 3,
+        "nivel" => 1,
+        "status" => $dados['status']
+    ];
+
+    $read = new \Conn\Read();
+    $read->exeRead("usuarios", "WHERE nome = :n", "n={$user['nome']}");
+    $user['nome'] = $user['nome'] . ($read->getResult() ? "-" . strtotime('now') : "");
+    $user["nome_usuario"] = \Helpers\Check::name($user['nome']);
+
+    $create = new \Conn\Create();
+    $create->exeCreate("usuarios", $user);
+
+    if($create->getResult()) {
+        $dados['usuario_id'] = $create->getResult();
+        $up = new \Conn\Update();
+        $up->exeUpdate("agentes", $dados, "WHERE id = :id", "id={$dados['id']}");
+    }
+}
